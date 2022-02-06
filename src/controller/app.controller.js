a8959a73c781df60b58e8681bfeb1a772f358b91
Jasurbek-Zaripov <95752012+Jasurbek-Zaripov@users.noreply.github.com>
2022-02-06 08:05:03 +0500

    Update app.controller.js

--- src/controller/app.controller.js
+++ src/controller/app.controller.js
@@ -79,8 +79,7 @@ export class AppController {
       upload(req, res, async err => {
         try {
           if (err) throw new Error(err)
-          console['log'](req.file)
-          console['log'](req.body)
+
           xato = await WriteFile(req.file)
 
           if (xato instanceof Error) {
